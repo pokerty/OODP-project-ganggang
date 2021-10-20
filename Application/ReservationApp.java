c24d08138211e4a51e7224ba661a251ba157cee5
pokerty <chenhongpo@gmail.com>
2021-10-21 07:43:11 +0800

    a

--- Application/ReservationApp.java
+++ Application/ReservationApp.java
@@ -7,36 +7,7 @@ import java.time.LocalDateTime;
 
 public class ReservationApp {
     public static void main(String[] args){
-        System.out.print(LocalDateTime.now());
-        boolean isRunning = true;
-        Scanner sc = new Scanner(System.in);
-        // Restaurant newRestaurant = new Restaurant(); 
-        int sel = 0;
-        do{
-            System.out.printf("Select an option\n" +
-                "(1) do something\n" +
-                "(2) \n" +
-                "(3) \n" +
-                "(4) \n" +
-                "(5) \n" +
-                "(6) \n" +
-                "(-1) Quit\n");
 
-            sel = sc.nextInt();
-            switch(sel){
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-            }}while (sel!=-1);
     }
     
 }
