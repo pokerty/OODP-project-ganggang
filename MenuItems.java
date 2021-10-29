148ecf47854d35e9eeeff2463ad0f4996509a574
pokerty <chenhongpo@gmail.com>
2021-10-30 04:17:05 +0800

    updated menuitems

--- MenuItems.java
+++ MenuItems.java
@@ -1,53 +1,68 @@
 public class MenuItems {
 
+	public enum courseType {
+		main, drinks, desserts, promotionSet
+	}
 	private String name;
+	private courseType category;
 	private String description;
 	private float price;
 
-	public MenuItems() {
+
+	public MenuItems(String name, courseType category, String description, float price) {
 		// TODO - implement MenuItems.MenuItems
+		this.name = name;
+		this.category = category;
+		this.description = description;
+		this.price = price;
 		throw new UnsupportedOperationException();
 	}
 
-	public void getName() {
-		// TODO - implement MenuItems.getName
-		throw new UnsupportedOperationException();
+	public String getName() {
+		return this.name;
 	}
-
 	/**
 	 * 
 	 * @param name
 	 */
-	public void setName(int name) {
-		// TODO - implement MenuItems.setName
+	public void setName(String name) {
+		this.name = name;
 		throw new UnsupportedOperationException();
 	}
 
-	public void getDescription() {
-		// TODO - implement MenuItems.getDescription
+	public courseType getCategory() {
+		return this.category;
+	}
+	/**
+	 * 
+	 * @param type
+	 */
+	public void setCategory(courseType category) {
+		this.category = category;
 		throw new UnsupportedOperationException();
 	}
 
+	public String getDescription() {
+		return this.description;
+	}
 	/**
 	 * 
 	 * @param description
 	 */
-	public void setDescription(int description) {
-		// TODO - implement MenuItems.setDescription
+	public void setDescription(String description) {
+		this.description = description;
 		throw new UnsupportedOperationException();
 	}
 
-	public void getPrice() {
-		// TODO - implement MenuItems.getPrice
-		throw new UnsupportedOperationException();
+	public float getPrice() {
+		return this.price;
 	}
-
 	/**
 	 * 
 	 * @param price
 	 */
-	public void setPrice(int price) {
-		// TODO - implement MenuItems.setPrice
+	public void setPrice(float price) {
+		this.price=price;
 		throw new UnsupportedOperationException();
 	}
 
