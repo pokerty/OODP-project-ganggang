217baea24d91c48dac0ab31537ff3f7b0105a47e
gohzy89 <gohzy89@gmail.com>
2021-10-31 20:19:33 +0800

    timer

--- MenuLogic.java
+++ MenuLogic.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList; 
+import java.util.ArrayList;
 import java.util.Scanner; 
 import MenuItems.courseType;
 import CourseType;
--- ReservationLogic.java
+++ ReservationLogic.java
@@ -1,8 +1,22 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class ReservationLogic {
 
 	private Reservation[] reservations;
 
 	public ReservationLogic() {
+		Runnable runnable = new Runnable() {
+
+			@Override
+			public void run() {
+				
+			}
+		};
+		final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+		scheduler.scheduleAtFixedRate(runnable, 10, 10, TimeUnit.SECONDS);
+
 		// TODO - implement ReservationLogic.ReservationLogic
 		throw new UnsupportedOperationException();
 	}
