9b66e4dd153e6d4d799ddd4599975dedd635a4a2
nownmin <kimminhwan57@gmail.com>
2021-11-05 16:52:03 +0800

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,24 +1,59 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class Order {
+    private int tableNumber;
+    private String timeStamp;
+    private ArrayList<MenuItems> orderItems;
+    private ArrayList<PromotionalSet> promoItems;
+    private boolean isMember;
+    private Staff staff;
 
-	private int tableNumber;
-	private int timestamp;
-	private MenuItems[] orderItems;
-	private boolean isMember;
-	private Staff staff;
+    public Order(int tableNumber, boolean isMember, Staff staff){
+        this.tableNumber = tableNumber;
+        this.timeStamp = Time.now();
+        ArrayList<MenuItems> orderArrayList = new ArrayList<MenuItems>;
+        this.orderItems = orderArrayList;
+        this.isMember = isMember;
+        this.staff = staff;
+    }
 
-	public Order() {
-		// TODO - implement Order.Order
-		throw new UnsupportedOperationException();
-	}
+    //get and set functions
+    public int getTableNumber() {
+        return tableNumber;
+    }
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
 
-	public void getOrderItems() {
-		// TODO - implement Order.getOrderItems
-		throw new UnsupportedOperationException();
-	}
+    public String getTimeStamp() {
+        return timeStamp;
+    }
 
-	public void printOrderDetails() {
-		// TODO - implement Order.printOrderDetails
-		throw new UnsupportedOperationException();
-	}
+    public ArrayList<PromotionalSet> getPromoItems() {
+        return promoItems;
+    }
+    public ArrayList<MenuItems> getOrderItems() {
+        return orderItems;
+    }
+    public void setOrderItems(ArrayList<MenuItems> orderItems, MenuItems menuItems) {
+        orderItems.add(menuItems);
+    }
+    public void removeOrderItems(ArrayList<MenuItems> orderItems, MenuItems menuItems) {
+        orderItems.remove(menuItems);
+    }
 
+    public Staff getStaff() {
+        return staff;
+    }
+    public void setStaff(Staff staff) {
+        this.staff = staff;
+    }
+
+    public boolean getMember() {
+        return isMember;
+    }
+    public void setMember(boolean member) {
+        isMember = member;
+    }
 }
