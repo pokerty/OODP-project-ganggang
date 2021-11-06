c0f5c1973d8adef43ed9af11e9b2c8db661f65c3
gohzy89 <91591202+gohzy89@users.noreply.github.com>
2021-11-06 16:07:26 +0800

    Update ReportBoundary.java

--- ReportBoundary.java
+++ ReportBoundary.java
@@ -1,18 +1,28 @@
+package oodpassignment;
+
 public class ReportBoundary {
+    public void reportTotal(ReportLogic reportLogic){
 
-	public void printRevenue() {
-		// TODO - implement ReportBoundary.printRevenue
-		throw new UnsupportedOperationException();
-	}
+    }
+    public void reportAlaCarte(ReportLogic reportLogic){
+        Report report = reportLogic.showAlaCarte();
+        System.out.println("Ala Carte Items Sold: ");
+        for(int i = 0; i<report.getOrders().size(); i++){
+            for(int j=0;j<report.getOrders().get(i).getOrderItems().size();j++) {
+                System.out.println(report.getOrders().get(i).getOrderItems().get(j).getName());
+            }
+        }
 
-	public void printSetPromo() {
-		// TODO - implement ReportBoundary.printSetPromo
-		throw new UnsupportedOperationException();
-	}
+    }
+    public void reportSetPromo(ReportLogic reportLogic){
+        Report report = reportLogic.showSetPromo();
+        System.out.println("Promotional Set Items sold: ");
+        for(int i = 0; i<report.getOrders().size(); i++){
+            for(int j=0;j<report.getOrders().get(i).getPromoItems().size();j++) {
+                System.out.println(report.getOrders().get(i).getPromoItems().get(j).getName());
+            }
+        }
 
-	public void printAlaCarte() {
-		// TODO - implement ReportBoundary.printAlaCarte
-		throw new UnsupportedOperationException();
-	}
+    }
 
 }
