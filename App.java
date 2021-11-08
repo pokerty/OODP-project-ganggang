db22aa78e247c94be33333167ee9013518b36f59
gohzy89 <91591202+gohzy89@users.noreply.github.com>
2021-11-08 22:13:38 +0800

    Add files via upload

--- App.java
+++ App.java
@@ -1,4 +1,4 @@
-package oodpassignment;
+package com.oodpassignment;
 
 import java.util.ArrayList;
 import java.util.concurrent.Executors;
@@ -8,24 +8,9 @@ import java.util.concurrent.TimeUnit;
 public class App {
 
 	public static void main(String[] args) {
-		CheckTable tableLogic = new TableLogic();
-		ReservationLogic reservationLogic = new ReservationLogic(tableLogic);
-		ArrayList<Reservation>reservations = reservationLogic.getReservations();
-		RestaurantApp restaurant = new RestaurantApp(); 
-		Runnable runnable = new Runnable() {
+		RestaurantApp restaurant = new RestaurantApp();
 
-			@Override
-			public void run() {
-				for(int i=0;i<reservations.size();i++){
-					if(System.currentTimeMillis()>=reservations.get(i).getDateandtime().getTimeInMillis()){
-						reservationLogic.removeReservation(reservations.get(i).getTableNumber(),reservations.get(i).getName(),true);
-					}
-				}
-			}
-		};
-		final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-		scheduler.scheduleAtFixedRate(runnable, 15, 15, TimeUnit.MINUTES);
-		restaurant.start(tableLogic,reservationLogic);
+		restaurant.start();
 
 
 	}
