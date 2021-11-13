ad762fc61994af7815b024ef8137b8a8f39533bc
CheeMeiQi <65078389+CheeMeiQi@users.noreply.github.com>
2021-11-13 22:03:12 +0800

    updated javadoc

--- oodpassignment/App.java
+++ oodpassignment/App.java
@@ -1,5 +1,12 @@
 package oodpassignment;
 
+/**
+ * Class with the main method
+ * @author Chang Wei
+ * @version 1.0
+ * @since 06/11/2021
+ *
+ */
 public class App {
 
 	public static void main(String[] args) {
