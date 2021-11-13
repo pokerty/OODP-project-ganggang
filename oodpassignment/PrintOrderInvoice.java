2d4b2b44ba3d5ecd6cda45470566bfce079c0629
CheeMeiQi <65078389+CheeMeiQi@users.noreply.github.com>
2021-11-13 22:07:47 +0800

    updated javadoc

--- oodpassignment/PrintOrderInvoice.java
+++ oodpassignment/PrintOrderInvoice.java
@@ -1,5 +1,13 @@
 package oodpassignment;
 
+/**
+ * An interface to be used by OrderLogic so as to print order items.
+ * 
+ * @author Chang Wei
+ * @version 1.0
+ * @since 06/11/2021
+ *
+ */
 public interface PrintOrderInvoice {
 
 	void printOrderInvoice();
